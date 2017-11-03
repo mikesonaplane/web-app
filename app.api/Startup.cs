2f429da83fa15a7ef2781f23aa93eb5e2eac7ee6
Mike McDonald <mkmcdona@gmail.com>
2017-11-03 14:26:58 -0700

    renamed to app.api to add app namespace for replacement later.

--- app.api/Startup.cs
+++ app.api/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace server
+namespace app.api
 {
     public class Startup
     {
