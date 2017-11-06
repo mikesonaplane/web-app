8804bdb6ad50659e333584a96c5e033b126351cb
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2017-11-06 12:18:24 -0800

    Adding required migration information.

--- App.API/Startup.cs
+++ App.API/Startup.cs
@@ -1,39 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PDX.PBOT.App.Data.Options;
 
 namespace PDX.PBOT.App.API
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var connectionString = Configuration.GetConnectionString("SqlServer");
+			var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			var storeOptions = new AppStoreOptions();
+			services.AddSingleton(storeOptions);
 
-            app.UseMvc();
-        }
-    }
+			services
+				.AddEntityFrameworkSqlServer()
+				.AddDbContext<Data.Concrete.AppContext>((serviceProvider, options) =>
+					options.UseSqlServer(connectionString, builder => builder.MigrationsAssembly(migrationsAssembly))
+						.UseInternalServiceProvider(serviceProvider));
+
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+		}
+	}
 }
