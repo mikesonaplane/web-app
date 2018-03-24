2c02e24a735182fc27e4b36ee45a36011fbd1861
Mike McDonald <mkmcdona@gmail.com>
2018-03-23 18:35:13 -0700

    Scrapping Angular version.

--- App.Web/src/app/interfaces/content.ts
+++
@@ -1,6 +0,0 @@
-export interface Content {
-    id: number;
-    lookupId: number;
-    lookupName: string;
-    data: string;
-}
--- App.Web/src/app/interfaces/lookup.ts
+++
@@ -1,4 +0,0 @@
-export interface Lookup {
-    id: number;
-    name: string;
-}
