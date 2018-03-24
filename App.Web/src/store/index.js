d1c1cb02e1b644b7cd34bb9aa5a2e3fe17958cb3
Mike McDonald <mkmcdona@gmail.com>
2018-03-24 09:31:02 -0700

    Added the beiggining of a store.

---
+++ App.Web/src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import content from './modules/content'
+import lookups from './modules/lookups'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  modules: {
+    cart,
+    products
+  },
+  strict: debug,
+})
